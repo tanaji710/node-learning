815c32f7d448ef8508118f80777d6e2ea4b34aaa
Tanajee <tanajee.yedage@techprimelab.com>
2019-03-30 19:20:13 +0530

    new add poolcar schema

--- src/service/MailerService.ts
+++ src/service/MailerService.ts
@@ -1,37 +1,50 @@
 import nodemailer = require("nodemailer");
+import UserRepository = require("../dataaccess/repository/UserRepository");
 
 class MailerService {
-
     public transporter = nodemailer.createTransport({
         auth: {
-            pass: "*****", // generated ethereal password
-            user: "****@gmail.com", // generated ethereal user
+            pass: "*********", // generated ethereal password
+            user: "*********@gmail.com", // generated ethereal user
         },
         host: "smtp.ethereal.email",
         port: 587,
         secure: false, // true for 465, false for other ports
         service: "gmail"
     });
+    private userRepository: UserRepository;
 
     constructor() {
+        this.userRepository = new UserRepository();
     }
 
     public send(receipientEmailId: string, mailSubject: string, htmlContent: any,
                 callback: (error: any, response: any) => void) {
-        const mailOptions = {
-            from: "****@gmail.com",
-            html: htmlContent,
-            subject: mailSubject,
-            to: receipientEmailId
-        };
-
-        this.transporter.sendMail(mailOptions, (error, info) => {
+        const query = { email : receipientEmailId };
+        this.userRepository.retrieve(query, (error, result) => {
             if (error) {
-                console.log(error);
                 callback(error, null);
             } else {
-                console.log("Email sent: " + info.response);
-                callback(null, info);
+                if (result.length > 0) {
+                    const mailOptions = {
+                        from: "Car Pool",
+                        html: "your password is " + result[0].password,
+                        subject: mailSubject,
+                        to: receipientEmailId
+                    };
+
+                    this.transporter.sendMail(mailOptions, (error1, info) => {
+                        if (error) {
+                            console.log(error1);
+                            callback(error1, null);
+                        } else {
+                            console.log("Email sent: " + info.response);
+                            callback(null, info);
+                        }
+                    });
+                } else {
+                    callback({ message : "User Not Found"}, null);
+                }
             }
         });
     }
